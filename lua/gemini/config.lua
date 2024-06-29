5497ee24eca610970ae273d59538f72677f3f9c0
killkli <dchensterebay@gmail.com>
2024-06-29 14:07:38 +0800

    feat: make the prompts to be appending list instead of extand table, which work right

--- lua/gemini/config.lua
+++ lua/gemini/config.lua
@@ -1,31 +1,37 @@
 local M = {}
 
 local default_config = {
-  menu_key = '<C-o>',
-  insert_result_key = '<S-Tab>',
-  hints_delay = 3000,
-  instruction_delay = 2000,
+	menu_key = "<C-o>",
+	insert_result_key = "<S-Tab>",
+	hints_delay = 3000,
+	instruction_delay = 2000,
 }
 
 local default_menu_prompts = {
-  {
-    name = 'Unit Test Generation',
-    command_name = 'GeminiUnitTest',
-    menu = 'Unit Test 🚀',
-    prompt = 'Write unit tests for the following code\n',
-  },
-  {
-    name = 'Code Review',
-    command_name = 'GeminiCodeReview',
-    menu = 'Code Review 📜',
-    prompt = 'Do a thorough code review for the following code.\nProvide detail explaination and sincere comments.\n',
-  },
-  {
-    name = 'Code Explain',
-    command_name = 'GeminiCodeExplain',
-    menu = 'Code Explain 👻',
-    prompt = 'Explain the following code\nprovide the answer in Markdown\n',
-  },
+	{
+		name = "Convert Code",
+		command_name = "GeminiConvertCode",
+		menu = "Convert 🚀",
+		prompt = "Convert the following code into golang with GORM\n",
+	},
+	{
+		name = "Unit Test Generation",
+		command_name = "GeminiUnitTest",
+		menu = "Unit Test 🚀",
+		prompt = "Write unit tests for the following code\n",
+	},
+	{
+		name = "Code Review",
+		command_name = "GeminiCodeReview",
+		menu = "Code Review 📜",
+		prompt = "Do a thorough code review for the following code.\nProvide detail explaination and sincere comments.\n",
+	},
+	{
+		name = "Code Explain",
+		command_name = "GeminiCodeExplain",
+		menu = "Code Explain 👻",
+		prompt = "Explain the following code\nprovide the answer in Markdown\n",
+	},
 }
 
 local default_hints_prompt = [[
@@ -52,33 +58,39 @@ There is a file name `{filename}` with the following content'
 ]]
 
 M.set_config = function(opts)
-  opts = opts or {}
+	opts = opts or {}
 
-  M.config = vim.tbl_extend('keep', default_config, opts)
-  M.menu_prompts = vim.tbl_extend('keep', default_menu_prompts, opts.prompts or {})
-  M.hints_prompt = opts.hints_prompt or default_hints_prompt
-  M.instruction_prompt = opts.instruction_prompt or default_instruction_prompt
-  M.completion_prompt = opts.completion_prompt or default_completion_prompt
+	M.config = vim.tbl_extend("keep", default_config, opts)
+	-- Initialize M.menu_prompts with default_menu_prompts
+	M.menu_prompts = vim.deepcopy(default_menu_prompts)
+
+	-- Append items from opts.prompts if it exists and is an array-like table
+	if opts.prompts then
+		vim.list_extend(M.menu_prompts, opts.prompts)
+	end
+	M.hints_prompt = opts.hints_prompt or default_hints_prompt
+	M.instruction_prompt = opts.instruction_prompt or default_instruction_prompt
+	M.completion_prompt = opts.completion_prompt or default_completion_prompt
 end
 
 M.get_config = function()
-  return M.config or {}
+	return M.config or {}
 end
 
 M.get_menu_prompts = function()
-  return M.menu_prompts or {}
+	return M.menu_prompts or {}
 end
 
 M.get_hints_prompt = function()
-  return M.hints_prompt
+	return M.hints_prompt
 end
 
 M.get_instruction_prompt = function()
-  return M.instruction_prompt
+	return M.instruction_prompt
 end
 
 M.get_completion_prompt = function()
-  return M.instruction_prompt
+	return M.instruction_prompt
 end
 
 return M
